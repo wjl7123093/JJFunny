803c1ef410da41587e3377dd9aaca0b767fb03d6
snow <wjl7123093@163.com>
2020-09-11 17:15:48 +0800

    create. 自定义 圆角容器 View 及 解析器。2020.9.11

---
+++ libcommon/src/main/java/com/snowapp/libcommon/view/CornerLinearLayout.java
@@ -0,0 +1,30 @@
+package com.snowapp.libcommon.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
+
+/**
+ * 自带圆角 LinearLayout
+ */
+public class CornerLinearLayout extends LinearLayout {
+    public CornerLinearLayout(Context context) {
+        this(context, null);
+    }
+
+    public CornerLinearLayout(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CornerLinearLayout(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        this(context, attrs, defStyleAttr, 0);
+    }
+
+    public CornerLinearLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+
+        ViewHelper.setViewOutline(this, attrs, defStyleAttr, defStyleRes);
+    }
+}
