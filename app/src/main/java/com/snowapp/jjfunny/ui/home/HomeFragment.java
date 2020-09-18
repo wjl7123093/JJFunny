004f01f89d3d08e5dea7daf60ec46b942ed86594
snow <wjl7123093@163.com>
2020-09-18 15:17:26 +0800

    create. 加入自动播放逻辑。2020.9.18

--- app/src/main/java/com/snowapp/jjfunny/ui/home/HomeFragment.java
+++ app/src/main/java/com/snowapp/jjfunny/ui/home/HomeFragment.java
@@ -1,12 +1,17 @@
 package com.snowapp.jjfunny.ui.home;
 
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.lifecycle.Observer;
 import androidx.paging.ItemKeyedDataSource;
 import androidx.paging.PagedList;
 import androidx.paging.PagedListAdapter;
 
 import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.snowapp.jjfunny.exoplayer.PageListPlayDetector;
 import com.snowapp.jjfunny.model.Feed;
 import com.snowapp.jjfunny.ui.AbsListFragment;
 import com.snowapp.jjfunny.ui.MutableDataSource;
@@ -17,9 +22,10 @@ import java.util.List;
 @FragmentDestination(pageUrl = "main/tabs/home", asStarter = true)
 public class HomeFragment extends AbsListFragment<Feed, HomeViewModel> {
     private static final String TAG = "HomeFragment";
+    private PageListPlayDetector playDetector ;
 
     @Override
-    protected void afterCreateView() {
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         mViewModel.getCacheLiveData().observe(this, new Observer<PagedList<Feed>>() {
             @Override
             public void onChanged(PagedList<Feed> feeds) {
@@ -27,12 +33,33 @@ public class HomeFragment extends AbsListFragment<Feed, HomeViewModel> {
                 adapter.submitList(feeds);
             }
         });
+        playDetector = new PageListPlayDetector(this, mRecyclerView);
+    }
+
+    @Override
+    protected void afterCreateView() {
     }
 
     @Override
     public PagedListAdapter getAdapter() {
         String feedType = getArguments() == null ? "all" : getArguments().getString("feedType");
-        return new FeedAdapter(getContext(), feedType);
+        return new FeedAdapter(getContext(), feedType){
+            // item 进入屏幕
+            @Override
+            public void onViewAttachedToWindow(@NonNull ViewHolder holder) {
+                super.onViewAttachedToWindow(holder);
+                if (holder.isVideoItem()) {
+                    playDetector.addTarget(holder.getListPlayerView());
+                }
+            }
+
+            // item 离开屏幕
+            @Override
+            public void onViewDetachedFromWindow(@NonNull ViewHolder holder) {
+                super.onViewDetachedFromWindow(holder);
+                playDetector.removeTarget(holder.getListPlayerView());
+            }
+        };
     }
 
     @Override
@@ -55,5 +82,17 @@ public class HomeFragment extends AbsListFragment<Feed, HomeViewModel> {
     @Override
     public void onRefresh(@NonNull RefreshLayout refreshLayout) {
         mViewModel.getDataSource().invalidate();
+    }
+
+    @Override
+    public void onPause() {
+        playDetector.onPause();
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        playDetector.onResume();
+        super.onResume();
     }
 }
