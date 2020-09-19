82d260120bb147e89a2156bebd65914fc8a8f2d4
snow <wjl7123093@163.com>
2020-09-19 15:43:17 +0800

    update. 2020.9.19

--- app/src/main/java/com/snowapp/jjfunny/JJApplication.java
+++ app/src/main/java/com/snowapp/jjfunny/JJApplication.java
@@ -9,8 +9,8 @@ public class JJApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-//        ApiService.init("http://123.56.232.18:8080/serverdemo", null);
+        ApiService.init("http://123.56.232.18:8080/serverdemo", null);
 //        ApiService.init("http://192.168.31.218:8080/serverdemo", null);
-        ApiService.init("http://192.168.101.103:8080/serverdemo", null);
+//        ApiService.init("http://192.168.101.103:8080/serverdemo", null);
     }
 }
