2d76889948641d7ad9559ab74d26ecc22e7ad6fb
snow <wjl7123093@163.com>
2020-09-30 16:35:40 +0800

    update. 新增转换观看人数方法。2020.9.30

--- app/src/main/java/com/snowapp/jjfunny/utils/StringConvert.java
+++ app/src/main/java/com/snowapp/jjfunny/utils/StringConvert.java
@@ -1,5 +1,13 @@
 package com.snowapp.jjfunny.utils;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.AbsoluteSizeSpan;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.StyleSpan;
+
 /**
  * @date 2020-08-21
  * @author snow
@@ -18,6 +26,24 @@ public class StringConvert {
             return String.valueOf(count);
         }
         return count / 10000 + "万";
+    }
+
+    // 转换观看人数
+    public static String convertTagFeedList(int num) {
+        if (num < 10000) {
+            return num + "人观看";
+        } else {
+            return num / 10000 + "万人观看";
+        }
+    }
+
+    public static CharSequence convertSpannable(int count, String intro) {
+        String countStr = String.valueOf(count);
+        SpannableString ss = new SpannableString(countStr + intro);
+        ss.setSpan(new ForegroundColorSpan(Color.BLACK), 0, countStr.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ss.setSpan(new AbsoluteSizeSpan(16, true), 0, countStr.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ss.setSpan(new StyleSpan(Typeface.BOLD), 0, countStr.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        return ss;
     }
 
 }
